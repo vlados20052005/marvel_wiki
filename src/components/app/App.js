0575da79ef2f80c848a3a06b01f48ae1d5cbaab7
vlados20052005 <152084287+vlados20052005@users.noreply.github.com>
2024-03-03 20:43:36 +0100

    Add files via upload

---
+++ src/components/app/App.js
@@ -0,0 +1,54 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+const SingleComicLayout = lazy(() =>
+  import("../pages/singleComicLayout/SingleComicLayout")
+);
+const SingleCharacterLayout = lazy(() =>
+  import("../pages/singleCharacterLayout/SingleCharacterLayout")
+);
+const SinglePage = lazy(() => import("../pages/SinglePage"));
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route end path="/" element={<MainPage />} />
+              <Route end path="/comics" element={<ComicsPage />} />
+              <Route
+                end
+                path="/comics/:id"
+                element={
+                  <SinglePage Component={SingleComicLayout} dataType="comic" />
+                }
+              />
+              <Route
+                end
+                path="/characters/:id"
+                element={
+                  <SinglePage
+                    Component={SingleCharacterLayout}
+                    dataType="character"
+                  />
+                }
+              />
+              <Route end path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,46 @@
+import { Link, NavLink, Outlet } from "react-router-dom";
+import "./appHeader.scss";
+
+const AppHeader = () => {
+  return (
+    <header className="app__header">
+      <h1 className="app__title">
+        <Link to="/">
+          <span>Marvel</span> information portal
+        </Link>
+      </h1>
+      <nav className="app__menu">
+        <ul>
+          <li>
+            <NavLink
+              end
+              style={({ isActive }) => ({
+                color: isActive ? "#9f0013" : "inherit",
+              })}
+              className="active"
+              to="/"
+            >
+              Characters
+            </NavLink>
+          </li>
+          /
+          <li>
+            <NavLink
+              end
+              style={({ isActive }) => ({
+                color: isActive ? "#9f0013" : "inherit",
+              })}
+              to="/comics"
+            >
+              Comics
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      {/* The Outlet component is used to render the child routes */}
+      <Outlet />
+    </header>
+  );
+};
+
+export default AppHeader;
